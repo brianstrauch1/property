499d0e994466747e5c23301a6a552d26e87b3dd8
brianstrauch1 <brianstrauch@gmail.com>
2026-02-21 15:36:34 -0500

    Update

--- redeploy.bat
+++ redeploy.bat
@@ -1,26 +1,4 @@
-@echo off
-echo.
-echo ===== Deploying Property App =====
-echo.
-
-cd /d C:\Users\brian\OneDrive\Desktop\property
-
-echo.
-echo Adding changes...
+cd C:\Users\brian\OneDrive\Desktop\property
 git add .
-
-echo.
-echo Committing...
-git commit -m "Auto deploy update"
-
-echo.
-echo Pushing to GitHub...
+git commit -m "Update"
 git push
-
-echo.
-echo Deploying to Vercel...
-vercel --prod
-
-echo.
-echo ===== Deployment Complete =====
-pause
